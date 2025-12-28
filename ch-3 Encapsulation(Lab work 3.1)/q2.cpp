1746281cbd26d1ec2741ccc888bc96caec4fcc9b
Charmee Paneliya <cpjstudy28@gmail.com>
2025-12-28 12:28:36 +0530

    some changes added

--- ch-3 Encapsulation(Lab work 3.1)/q2.cpp
+++ ch-3 Encapsulation(Lab work 3.1)/q2.cpp
@@ -11,36 +11,21 @@ class Customer{
 
    public:
    
-        void getData(){
+        void setData(int id,string name,int age,string city,int mobile_number,int simcard_validity,string telecom_brand_name){
 
-            cout<<"Customer id: ";
-            cin>>cust_id;
-            cin.ignore();
+            this->cust_id = id;
+            this->cust_name = name;
+            this->cust_age = age;
+            this->cust_city = city;
+            this->cust_mobile_number = mobile_number;
+            this->cust_simcard_validity = simcard_validity;
+            this->cust_telecom_brand_name = telecom_brand_name;
 
-            cout<<"Customer name: ";
-            getline(cin,cust_name);
-
-            cout<<"Customer age: ";
-            cin>>cust_age;
-            cin.ignore();
-
-            cout<<"Customer city: ";
-            getline(cin,cust_city);
-
-            cout<<"Customer mobile number: ";
-            cin>>cust_mobile_number;
-            cin.ignore();
-
-            cout<<"Customer simcard validity: ";
-            cin>>cust_simcard_validity;
-            cin.ignore();
-
-            cout<<"Customer telecom brand name: ";
-            getline(cin,cust_telecom_brand_name);
+           
 
         }
 
-        void setData(){
+        void getData(){
 
             cout<<"student details........";
 
@@ -56,18 +41,44 @@ class Customer{
 
 int main(){
 
-    int cust_id,cust_age,cust_mobile_number,cust_simcard_validity;
-    string cust_name,cust_city,cust_telecom_brand_name;
+    int id,age,mobile_number,simcard_validity;
+    string name,city,telecom_brand_name;
 
     Customer c[2];
 
     for(int i=0; i<2; i++){
         cout<<"Enter details of customer: "<<i+1<<endl;
-        c[i].getData();
+
+         cout<<"Customer id: ";
+            cin>>id;
+            cin.ignore();
+
+            cout<<"Customer name: ";
+            getline(cin,name);
+
+            cout<<"Customer age: ";
+            cin>>age;
+            cin.ignore();
+
+            cout<<"Customer city: ";
+            getline(cin,city);
+
+            cout<<"Customer mobile number: ";
+            cin>>mobile_number;
+            cin.ignore();
+
+            cout<<"Customer simcard validity: ";
+            cin>>simcard_validity;
+            cin.ignore();
+
+            cout<<"Customer telecom brand name: ";
+            getline(cin,telecom_brand_name);
+
+            c[i].setData(id,name,age,city,mobile_number,simcard_validity,telecom_brand_name);
     }
 
     for(int i=0; i<2; i++){
-        c[i].setData();
+        c[i].getData();
     }
     return 0;
 }
